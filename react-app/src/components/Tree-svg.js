12f2d13b5a1000933b425ef7f4196c06690c7d0a
philsometimes <philsometimes@gmail.com>
2020-04-02 14:47:06 -0400

    fixed wrong tree for lab 7

--- react-app/src/components/Tree-svg.js
+++ react-app/src/components/Tree-svg.js
@@ -2,6 +2,7 @@ import React, {useContext, useEffect, useState} from 'react';
 import Modal from 'styled-react-modal'
 import { ModalProvider } from 'styled-react-modal'
 import {ReactComponent as Icon} from '../assets/tree-icon.svg'
+import tree7svg from '../data/trees/tree7.svg';
 import tree6svg from '../data/trees/tree6.svg';
 import tree5svg from '../data/trees/tree5.svg';
 import PhyloCanvas from './PhyloCanvas';
@@ -42,7 +43,7 @@ const Tree = (props) => {
             onBackgroundClick={toggleShow}
             onEscapeKeydown={toggleShow}>
 
-                <img src={tree5svg} style={{width:'100%', height:'100%'}} alt="Tree 6" />
+                <img src={tree7svg} style={{width:'100%', height:'100%'}} alt="Tree 7" />
 
           </TreeModal>
     </ModalProvider>
