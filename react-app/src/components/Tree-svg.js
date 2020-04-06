8b2e889db032da584c5a860f8f2555cf729e7458
philsometimes <philsometimes@gmail.com>
2020-04-05 20:47:34 -0400

    fixed tree svg loading

--- react-app/src/components/Tree-svg.js
+++ react-app/src/components/Tree-svg.js
@@ -2,13 +2,9 @@ import React, {useContext, useEffect, useState} from 'react';
 import Modal from 'styled-react-modal'
 import { ModalProvider } from 'styled-react-modal'
 import {ReactComponent as Icon} from '../assets/tree-icon.svg'
-import Tree7svg from '../data/trees/tree7.svg';
-import Tree6svg from '../data/trees/tree6.svg';
-import Tree5svg from '../data/trees/tree5.svg';
 import PhyloCanvas from './PhyloCanvas';
 import { labContext } from '../data/DataContexts';
 import labList from '../data/labList';
-
 
 
 
@@ -29,20 +25,14 @@ const Tree = (props) => {
   const {lab, setLab} = useContext(labContext);
   const [show, setShow] = useState(false);
   const [tree, setTree] = useState(labList[lab]["tree"]);
+
   function toggleShow(){
     setShow(!show)
   };
   useEffect (() => {
     setTree(labList[lab]["tree"])
   },[lab])
-  let treeToDisplay = null
-  if (lab=="Lab 5") {
-    treeToDisplay = Tree5svg
-  } else if (lab=="Lab 6"){
-    treeToDisplay = Tree6svg
-  } else if (lab =="Lab 7"){
-    treeToDisplay = Tree7svg
-  }
+
   return (
       <ModalProvider>
         <Icon width={props.iconSize} onClick={toggleShow} style={{cursor:'pointer'}} />
@@ -50,9 +40,7 @@ const Tree = (props) => {
             isOpen={show}
             onBackgroundClick={toggleShow}
             onEscapeKeydown={toggleShow}>
-
-                <img src={treeToDisplay} style={{width:'100%', height:'100%'}} alt="Tree 7" />
-
+                <img src={require("../data/trees/"+tree)} style={{width:'100%', height:'100%'}} />
           </TreeModal>
     </ModalProvider>
   )
